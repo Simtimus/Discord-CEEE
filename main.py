80538235a5b5ecb94c03e1e05cd985ceef399328
Simtimus <simtimusbos@gmail.com>
2021-10-29 13:19:03 +0300

    Am scos functia embeded

--- main.py
+++ main.py
@@ -5,11 +5,13 @@ import discord
 import asyncio
 import datetime
 from datetime import date
-import database
-import config
 from discord.ext import commands
 from discord_components import *
 # from discord_slash import SlashCommand
+
+# Importarea fisierelor
+import database
+import config
 
 # Initializarea discord
 intents = discord.Intents.all()
@@ -17,7 +19,6 @@ client = commands.Bot(command_prefix=config.cmd_prefix, case_insensitive=True, i
 # slash = SlashCommand(client, sync_commands=True)
 
 SchoolDB = database.DBlib(host=config.school_host, user=config.school_user, password=config.school_password, database=config.school_database)
-cog_names = []
 
 
 @client.event
@@ -26,10 +27,9 @@ async def on_ready():
 	print(f'Connected as {client.user.name} (ID:{client.user.id})')
 
 
-for filename in os.listdir('cogs'):
+for filename in os.listdir('./cogies'):
 	if filename.endswith('.py'):
-		client.load_extension(f'cogs.{filename[:-3]}')
-		cog_names.append(filename[:-3])
+		client.load_extension(f'cogies.{filename[:-3]}')
 
 if not config.is_local_run:
 	import keep_alive
